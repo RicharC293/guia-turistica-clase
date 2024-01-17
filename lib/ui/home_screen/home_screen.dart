7b5b1d5042c9de7fdec84c6f46f334f26725baee
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-01-16 21:10:28 -0500

    Add category images to home screen

--- lib/ui/home_screen/home_screen.dart
+++ lib/ui/home_screen/home_screen.dart
@@ -80,6 +80,44 @@ class HomeScreen extends StatelessWidget {
                 itemCount: 15,
               ),
             ),
+            Text('Categorias', style: Theme.of(context).textTheme.titleMedium),
+            SizedBox(
+              // definir una altura
+              height: 160,
+              child: ListView.separated(
+                padding: const EdgeInsets.all(16),
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.network(
+                          'https://st.depositphotos.com/1016440/2534/i/450/depositphotos_25344733-stock-photo-sunrise-at-the-beach.jpg',
+                          // Responsive
+                          width: MediaQuery.of(context).size.width * 0.25,
+                          height: MediaQuery.of(context).size.width * 0.25,
+                          // Como se renderiza la imagen
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        'Categoria',
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelSmall
+                            ?.copyWith(fontSize: 10),
+                      ),
+                    ],
+                  );
+                },
+                separatorBuilder: (context, index) => const SizedBox(width: 16),
+                itemCount: 6,
+                scrollDirection: Axis.horizontal,
+              ),
+            ),
+
+            
           ],
         ),
       ),
