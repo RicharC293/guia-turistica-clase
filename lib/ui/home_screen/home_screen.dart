89bedaac6f9db858cee2fe27c9a46f1b394b61f0
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-01-16 20:26:00 -0500

    Refactor home screen layout and add search button

--- lib/ui/home_screen/home_screen.dart
+++ lib/ui/home_screen/home_screen.dart
@@ -9,16 +9,16 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Row(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
                 children: [
-                  CircleAvatar(
+                  const CircleAvatar(
                     child: Text('RC'),
                   ),
-                  SizedBox(width: 16),
+                  const SizedBox(width: 16),
                   Expanded(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -32,12 +32,55 @@ class HomeScreen extends StatelessWidget {
                       ],
                     ),
                   ),
-                  SizedBox(width: 16),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+                  const SizedBox(width: 16),
+                  OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      minimumSize: const Size(50, 50),
+                      backgroundColor: Colors.white,
+                      padding: EdgeInsets.zero,
+                      elevation: 1,
+                    ),
+                    child: const Icon(Icons.search, size: 30),
+                  ),
                 ],
               ),
-            ],
-          ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                left: 16,
+                bottom: 16,
+              ),
+              padding: const EdgeInsets.only(
+                left: 4,
+                top: 6,
+                bottom: 6,
+              ),
+              decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.background,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(8),
+                    bottomLeft: Radius.circular(8),
+                  )),
+              height: 50,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text('index: $index',
+                          style: Theme.of(context).textTheme.labelLarge),
+                    ),
+                  );
+                },
+                itemCount: 15,
+              ),
+            ),
+          ],
         ),
       ),
     );
