da1ab788d0857b4ef0b79efa2a0fa262cc5d9eac
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-01-17 21:09:08 -0500

    Add details and reservation functionality to DetailScreen

--- lib/ui/details_screen/detail_screen.dart
+++ lib/ui/details_screen/detail_screen.dart
@@ -12,7 +12,54 @@ class DetailScreen extends StatelessWidget {
       body: Column(
         children: [
           Header(),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Mitad del mundo',
+                    style: Theme.of(context).textTheme.titleLarge),
+                const SizedBox(height: 4),
+                Row(
+                  children: [
+                    Row(
+                      children: List.generate(
+                          5,
+                          (index) => const Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                                size: 16,
+                              )),
+                    ),
+                    Text('5'),
+                  ],
+                ),
+                SelectableText(
+                  'Descripcion del atractivo turistico, Descripcion del atractivo turistico,Descripcion del atractivo turistico,Descripcion del atractivo turistico,Descripcion del atractivo turistico,Descripcion del atractivo turistico,Descripcion del atractivo turistico',
+                  style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('Copiar descripción',
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelSmall
+                            ?.copyWith(fontSize: 10)),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: Text('Reservar'),
+        icon: Icon(Icons.calendar_month),
       ),
     );
   }
